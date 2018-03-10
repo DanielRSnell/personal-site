45bc649949b9f720711732adab2f74c267b33b50
DanielRSnell <hi@danielsnell.ninja>
2018-03-10 17:36:19 -0500

    update project files for previews

--- src/cms/preview-templates/HomePagePreview.js
+++ src/cms/preview-templates/HomePagePreview.js
@@ -3,7 +3,7 @@ import { HomePageTemplate } from "../../templates/home-page";
 
 const HomePagePreview = ({ entry, widgetFor }) => (
   <HomePageTemplate
-    content={widgetFor("content")}
+    content={widgetFor("body")}
     description={entry.getIn(["data", "description"])}
     title={entry.getIn(["data", "title"])}
   />
