d45edd68f1c201021caf7380ba895cf9df09a7fc
DanielRSnell <hi@danielsnell.ninja>
2018-03-10 22:51:52 -0500

    update home page template

--- src/templates/home-page.js
+++ src/templates/home-page.js
@@ -1,9 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
+import Content, { HTMLContent } from "../components/Content";
 
-class Home extends Component {
-  render() {
-    return <div className="example">This is an example page...</div>;
+export const HomePageTemplate = ({ title, content, contentComponent }) => {
+  const PageContent = contentComponent || Content;
+
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {title}
+              </h2>
+              <PageContent className="content" content={content} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ({ data }) => {
+  const { markdownRemark: post } = data;
+  console.log(data);
+  return (
+    <HomePageTemplate
+      contentComponent={HTMLContent}
+      title={post.frontmatter.title}
+      content={post.html}
+    />
+  );
+};
+
+export const homePageQuery = graphql`
+  query HomePage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+      }
+    }
   }
-}
-
-export default Home;
+`;
