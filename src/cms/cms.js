a2166ae5ee8358ab882cd1d03c8b13b8f99dc231
DanielRSnell <hi@danielsnell.ninja>
2018-03-10 23:31:57 -0500

    update index.md - home

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,7 +4,7 @@ import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import HomePagePreview from "./preview-templates/HomePagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 
-CMS.registerPreviewStyle("/styles.css");
+CMS.registerPreviewStyle("../layouts/sass/main.scss");
 CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("home", HomePagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
