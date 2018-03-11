acc6eee435757c55751a8ba95ea5d5e56ff0a4eb
DanielRSnell <hi@danielsnell.ninja>
2018-03-11 00:11:38 -0500

    update body preview

--- src/cms/preview-templates/HomePagePreview.js
+++ src/cms/preview-templates/HomePagePreview.js
@@ -2,7 +2,10 @@ import React from "react";
 import { HomePageTemplate } from "../../templates/home-page";
 
 const HomePagePreview = ({ entry, widgetFor }) => (
-  <HomePageTemplate title={entry.getIn(["data", "title"])} />
+  <HomePageTemplate
+    title={entry.getIn(["data", "title"])}
+    content={widgetFor("body")}
+  />
 );
 
 export default HomePagePreview;
